0d4f099e41305962a5d4723cbf637c1f370fc96b
Eliska-ls <114865501+Eliska-ls@users.noreply.github.com>
2022-10-31 10:25:32 +0300

    kayıt ekranı

---
+++ ödev1/1.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Form Uygulamalari</title>
+</head>
+<body>
+<?php
+echo "<h1> Giriş ekranı</h1>";
+?>
+<form action="giriş.php" method="post">
+ <input type="submit" value="Login" />
+ <br> </br>
+ <input type="submit" value="Sign" />
+</form>
+</body>
+</html>
---
+++ ödev1/anasayfa.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<?php
+echo "<h1> Kilit Ekranı  </h1>";
+?>
+<p> Giriş Başarılı</p>
+<form action="1.php" method="post">
+ <input type="submit" value=" Başa Dön  " />
+</form>
+</body>
+</html>
